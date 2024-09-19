3498bb75a8ccb4ad0917daa23029e627f7edf000
Gelan Balcha <tuo96713@temple.edu>
2024-09-19 12:23:29 -0400

    made last update

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -22,7 +22,7 @@ class TextSizeAdapter (private val _context: Context, private val _numbers: Arra
     }
 
     override fun getItemId(position: Int): Long {
-        return numbers[position].toLong()
+        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
@@ -31,12 +31,13 @@ class TextSizeAdapter (private val _context: Context, private val _numbers: Arra
             textView = convertView as TextView
         } else {
             textView = TextView(context)
-            textView.textSize = 22f
             textView.setPadding(5,10,0,10)
         }
 
+        textView.textSize = (numbers[position].toFloat())
+        textView.text = numbers[position].toString()
+
         return textView
     }
-
 
 }
